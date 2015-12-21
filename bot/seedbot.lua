6203d9224cfcdb7b236f71cf805dc23e1a8d7103
mohsen9hard <mohsen9hard@gmail.com>
2015-12-21 21:02:49 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users ={ 140660762 ,tonumber(our_id)},--Sudo users
+    sudo_users ={140660762},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
